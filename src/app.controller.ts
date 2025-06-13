dceb46de92b43c39712c4b758fc0bd853329142f
mike-ayres <michael.l.ayres+oneme@gmail.com>
2025-06-13 23:52:57 +0000

    converted to fastify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,29 +11,30 @@ export class AppController {
     }
 
     // this works with or without trailing slash
-    @Get('sample{/:exampleParam}')
+    @Get('sample/:exampleParam?')
     public getWithOptionalRoute(@Param('exampleParam') exampleParam?: string): string {
         return this.appService.getHelloName(exampleParam);
     }
 
-    // this does not, you need a trailing slash for this route to work
-    @Get('sample2/{:exampleParam}')
+    // this works the same as sample 1 in fastify
+    @Get('sample2/:exampleParam?')
     public getWithSlashBeforeOptionalRoute(@Param('exampleParam') exampleParam?: string): string {
         return this.appService.getHelloName(exampleParam);
     }
 
-    // this does not, you need a trailing slash for this route to work
-    @Get('sample3{/:exampleParam1}{/:exampleParam2}')
+    // You need a trailing slash for this route to work (e.g., `sample3/`) because
+    //  fastify only supports one optional segment (the last one)
+    @Get('sample3/:exampleParam1/:exampleParam2?')
     public getMultipleOptionalRouteSegments(
-        @Param('exampleParam1') exampleParam1?: string,
+        @Param('exampleParam1') exampleParam1: string,
         @Param('exampleParam2') exampleParam2?: string
     ): string {
         const fullName: string = (exampleParam1 ?? '') + ' ' + (exampleParam2 ?? '');
         return this.appService.getHelloName(fullName.trim());
     }
 
-    // this does not, you need a trailing slash for this route to work
-    @Get('sample4{/{:exampleParam1}{/:exampleParam2}}')
+    // same as sample3, removed the second optional route that the express version has.
+    @Get('sample4/:exampleParam1/:exampleParam2?')
     public getMultipleOptionalRouteSegments2(
         @Param('exampleParam1') exampleParam1?: string,
         @Param('exampleParam2') exampleParam2?: string
